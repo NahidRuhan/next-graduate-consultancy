90bb9ae65c4c313797da1a93a35a52ff2b9843ef
NahidRuhan <dilal328@gmail.com>
2024-03-20 01:57:33 +0600

    nav bar worked sprisingly and added service pages

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/shared/Footer";
+import Nav from "@/components/shared/Nav";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Nav></Nav>
         {children}
         <Footer></Footer>
       </body>
