a9de6f57c827c693497c19bcb18757ce09b88bdd
NahidRuhan <dilal328@gmail.com>
2024-03-20 01:41:42 +0600

    going smooth

--- app/Home.jsx
+++ app/Home.jsx
@@ -1,6 +1,9 @@
+import AnimatedCount from "@/components/home/AnimatedCount";
 import Banner from "@/components/home/Banner";
 import Country from "@/components/home/Country";
 import HomeAbout from "@/components/home/HomeAbout";
+import HomePartner from "@/components/home/HomePartner";
+import HomeServices from "@/components/home/HomeServices";
 
 const Home = () => {
     return (
@@ -8,9 +11,9 @@ const Home = () => {
             <Banner></Banner>
             <Country></Country>
             <HomeAbout></HomeAbout>
-            {/* <HomeServices></HomeServices> */}
-            {/* <HomePartner></HomePartner> */}
-            {/* <AnimatedCount></AnimatedCount> */}
+            <HomeServices></HomeServices>
+            <HomePartner></HomePartner>
+            <AnimatedCount></AnimatedCount>
             {/* <Socials></Socials> */}
         </div>
     );
--- components/home/HomeAbout.jsx
+++ components/home/HomeAbout.jsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import image from "@/public/assets/home/1.jpg"
+import { TiTick } from "react-icons/ti";
+import { IoIosFastforward } from "react-icons/io";
 
 
 const HomeAbout = () => {
@@ -14,14 +16,14 @@ const HomeAbout = () => {
             <div className="flex flex-col gap-2">
     <h1 className="text-3xl pb-2 font-bold">About Graduate Consultancy</h1>
     <p><i className="fa-solid fa-check text-red-500"></i> Tailored mentorship for graduate&apos;s academic journey and visa processes. </p>
-                <p><i className="fa-solid fa-check text-red-500"></i> Customized consultancy covering global educational programs and institutions comprehensively. </p>
-                <p><i className="fa-solid fa-check text-red-500"></i> Proficient guidance simplifies intricate visa applications for smooth transitions. </p>
-                <p><i className="fa-solid fa-check text-red-500"></i> Accessible international education through worldwide connections and local adeptness. </p>
-                <p><i className="fa-solid fa-check text-red-500"></i> Dedicated support ensures the achievement of academic and career aspirations. </p>
+                <p className="flex items-center gap-2"><span className="text-red-500"><TiTick /></span> Customized consultancy covering global educational programs and institutions comprehensively. </p>
+                <p className="flex items-center gap-2"><span className="text-red-500"><TiTick /></span> Proficient guidance simplifies intricate visa applications for smooth transitions. </p>
+                <p className="flex items-center gap-2"><span className="text-red-500"><TiTick /></span> Accessible international education through worldwide connections and local adeptness. </p>
+                <p className="flex items-center gap-2"><span className="text-red-500"><TiTick /></span> Dedicated support ensures the achievement of academic and career aspirations. </p>
     <div className="flex justify-center lg:justify-start"> {/* Added flex and justify-center classes */}
         <Link href="/about-us">
             <button className="btn btn-error mt-10 font-bold">
-                About Us <i className="fa-solid fa-forward"></i>
+                About Us <IoIosFastforward />
             </button>
         </Link>
     </div>
