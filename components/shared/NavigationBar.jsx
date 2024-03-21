c1bd918757c2eceb1e53c7bc068e6a30b448bc8e
NahidRuhan <dilal328@gmail.com>
2024-03-21 16:20:03 +0600

    fixed broken link 2

--- components/shared/NavigationBar.jsx
+++ components/shared/NavigationBar.jsx
@@ -48,8 +48,8 @@ const NavigationBar = () => {
       {name:'Pre-Departure Guidance', path: '/services/pre-departure-guidance'},
     ] },
       { title: 'Study Abroad', path: '/study-abroad', hasSubNav: true, subNav: [{name: 'Canada', path:'/study-abroad/canada'}, {name:'USA', path: '/study-abroad/usa'}, {name:'UK', path:'/study-abroad/uk'}, {name:'Australia', path:'/study-abroad/australia'}] },
-      { title: 'Study Canada', path: '/study-canada', hasSubNav: false },
-      { title: 'About Us', path: '/about-us', hasSubNav: true, subNav: [{name:'About Graduate Consultancy', path:'/'}, {name: 'Our Vision & Mission', path: '/vision-mission'}, {name: 'Meet Our Team', path: '/meet-our-team'}] },
+      { title: 'Study Canada', path: '/study-abroad/canada', hasSubNav: false },
+      { title: 'About Us', path: '/about-us', hasSubNav: true, subNav: [{name:'About Graduate Consultancy', path:'/about-us'}, {name: 'Our Vision & Mission', path: '/about-us/vision-mission'}, {name: 'Meet Our Team', path: '/meet-our-team'}] },
       { title: 'Contact Us', path: '/contact-us', hasSubNav: false },
     ];
   
