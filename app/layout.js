fb271d97ff70c1c33ecbb4b8c19ccbc34e412af7
NahidRuhan <dilal328@gmail.com>
2024-03-21 22:46:41 +0600

    added speed insight

--- app/layout.js
+++ app/layout.js
@@ -20,6 +20,7 @@ export default function RootLayout({ children }) {
         <NavigationBar></NavigationBar>
         {children}
         <Footer></Footer>
+        <SpeedInsights />
       </body>
     </html>
   );
